1df67e8bd219772503d9316417b00ce6a80427b8
牧仁 <xuqi.xq@alibaba-inc.com>
2015-10-19 10:24:07 +0800

    bugfix:parse error when download source page fail

--- stock/src/main/java/com/zhaijiong/stock/provider/DailyDataProvider.java
+++ stock/src/main/java/com/zhaijiong/stock/provider/DailyDataProvider.java
@@ -45,72 +45,80 @@ public class DailyDataProvider {
 
     /**
      * 获取前复权数据
+     *
      * @param symbol
      * @param startDate
      * @param stopDate
      * @return
      */
-    private static Map<String,StockData> qfqData(String symbol, String startDate, String stopDate){
-        Map<String,StockData> stockDataMap = Maps.newHashMap();
+    private static Map<String, StockData> qfqData(String symbol, String startDate, String stopDate) {
+        Map<String, StockData> stockDataMap = Maps.newHashMap();
 
-        String hfqURL = String.format(DAILY_HFQ_URL,symbol);
-        String data = Downloader.download(hfqURL,"gb2312");
-        Elements select = Jsoup.parse(data).getElementById("con02-4").getElementsByTag("select").get(0).getElementsByTag("option");
-        List<String> pages = Lists.newArrayListWithCapacity(50);
-        for(Element option:select){
-            pages.add(option.text()+""+"1231");
-            pages.add(option.text()+""+"0930");
-            pages.add(option.text()+""+"0630");
-            pages.add(option.text()+""+"0331");
-        }
-        Collections.reverse(pages);
+        String hfqURL = String.format(DAILY_HFQ_URL, symbol);
+        String data = Downloader.download(hfqURL, "gb2312");
+        try {
+            Elements select = Jsoup.parse(data).getElementById("con02-4").getElementsByTag("select").get(0).getElementsByTag("option");
+            List<String> pages = Lists.newArrayListWithCapacity(50);
+            for (Element option : select) {
+                pages.add(option.text() + "" + "1231");
+                pages.add(option.text() + "" + "0930");
+                pages.add(option.text() + "" + "0630");
+                pages.add(option.text() + "" + "0331");
+            }
+            Collections.reverse(pages);
 
-        for(String page:pages){
-            Date date = Utils.str2Date(page,"yyyyMMdd");
-            //TODO date < stop
-            if(date.getTime() > Utils.str2Date(startDate,"yyyyMMdd").getTime() /*&& date.getTime() < Utils.str2Date(stopDate,"yyyyMMdd").getTime()*/){
-                String url = String.format(DAILY_HFQ_PARAM_URL,symbol,Utils.formatDate(date,"yyyy"),getQuarter(date));
-                data = Downloader.download(url,"gb2312");
-                Elements tr = Jsoup.parse(data).getElementById("FundHoldSharesTable").getElementsByTag("tbody").get(0).getElementsByTag("tr");
-                for(int i = 1;i<tr.size();i++){
-                    Elements td = tr.get(i).getElementsByTag("td");
-                    Date stockDate = Utils.str2Date(td.get(0).text(),"yyyy-MM-dd");
-                    if(stockDate.getTime()>= Utils.str2Date(startDate,"yyyyMMdd").getTime() && stockDate.getTime() <= Utils.str2Date(stopDate,"yyyyMMdd").getTime()){
-                        StockData stockData = new StockData(symbol);
 
-                        stockData.date = Utils.str2Date(td.get(0).text(),"yyyy-MM-dd");
-                        stockData.put(StockConstants.OPEN,Double.parseDouble(td.get(1).text()));
-                        stockData.put(StockConstants.HIGH,Double.parseDouble(td.get(2).text()));
-                        stockData.put(CLOSE,Double.parseDouble(td.get(3).text()));
-                        stockData.put(StockConstants.LOW,Double.parseDouble(td.get(4).text()));
-                        stockData.put(StockConstants.VOLUME,Double.parseDouble(td.get(5).text())/100);  //单位：手
-                        stockData.put(StockConstants.AMOUNT,Double.parseDouble(td.get(6).text())/10000);//单位：万
-                        stockData.put(StockConstants.FACTOR,Double.parseDouble(td.get(7).text()));
-                        stockDataMap.put(td.get(0).text().replaceAll("-", ""), stockData);
+            for (String page : pages) {
+                Date date = Utils.str2Date(page, "yyyyMMdd");
+                //TODO date < stop
+                if (date.getTime() > Utils.str2Date(startDate, "yyyyMMdd").getTime() /*&& date.getTime() < Utils.str2Date(stopDate,"yyyyMMdd").getTime()*/) {
+                    String url = String.format(DAILY_HFQ_PARAM_URL, symbol, Utils.formatDate(date, "yyyy"), getQuarter(date));
+                    data = Downloader.download(url, "gb2312");
+                    Elements tr = Jsoup.parse(data).getElementById("FundHoldSharesTable").getElementsByTag("tbody").get(0).getElementsByTag("tr");
+                    for (int i = 1; i < tr.size(); i++) {
+                        Elements td = tr.get(i).getElementsByTag("td");
+                        Date stockDate = Utils.str2Date(td.get(0).text(), "yyyy-MM-dd");
+                        if (stockDate.getTime() >= Utils.str2Date(startDate, "yyyyMMdd").getTime() && stockDate.getTime() <= Utils.str2Date(stopDate, "yyyyMMdd").getTime()) {
+                            StockData stockData = new StockData(symbol);
+
+                            stockData.date = Utils.str2Date(td.get(0).text(), "yyyy-MM-dd");
+                            stockData.put(StockConstants.OPEN, Double.parseDouble(td.get(1).text()));
+                            stockData.put(StockConstants.HIGH, Double.parseDouble(td.get(2).text()));
+                            stockData.put(CLOSE, Double.parseDouble(td.get(3).text()));
+                            stockData.put(StockConstants.LOW, Double.parseDouble(td.get(4).text()));
+                            stockData.put(StockConstants.VOLUME, Double.parseDouble(td.get(5).text()) / 100);  //单位：手
+                            stockData.put(StockConstants.AMOUNT, Double.parseDouble(td.get(6).text()) / 10000);//单位：万
+                            stockData.put(StockConstants.FACTOR, Double.parseDouble(td.get(7).text()));
+                            stockDataMap.put(td.get(0).text().replaceAll("-", ""), stockData);
+                        }
                     }
                 }
             }
+        } catch (Exception e) {
+            LOG.error("fail to get from url " + hfqURL);
         }
+
         return stockDataMap;
     }
 
     /**
      * 根据时间获取季度
+     *
      * @param date
      * @return
      */
-    public static int getQuarter(Date date){
-        return (date.getMonth()/3)+1;
+    public static int getQuarter(Date date) {
+        return (date.getMonth() / 3) + 1;
     }
 
     private static List<StockData> getDailyDataWithOutFQ(String symbol, String startDate, String stopDate) {
-        String url = getPath(symbol,startDate,stopDate);
+        String url = getPath(symbol, startDate, stopDate);
         List<StockData> stocks = Lists.newLinkedList();
         try {
             String data = Downloader.download(url, "gb2312");
             String[] lines = data.split("\n");
 
-            for(int i=1;i<lines.length;i++){    //第一行是标题，跳过
+            for (int i = 1; i < lines.length; i++) {    //第一行是标题，跳过
                 String[] line = lines[i].split(",");
                 if (line.length == 15 && !lines[i].contains("None")) {
                     try {
@@ -118,10 +126,10 @@ public class DailyDataProvider {
 
                         stock.date = Utils.str2Date(line[0], Constants.NETEASE_DATE_STYLE);
                         stock.name = line[2];
-                        for(int j = 0 ;j<DAILY.size()-1;j++){
-                            stock.put(DAILY.get(j),Utils.str2Double(line[j+3]));
+                        for (int j = 0; j < DAILY.size() - 1; j++) {
+                            stock.put(DAILY.get(j), Utils.str2Double(line[j + 3]));
                         }
-                        stock.put("amplitude",Utils.formatDouble((stock.get("high") - stock.get("low")) / stock.get("lastClose")));
+                        stock.put("amplitude", Utils.formatDouble((stock.get("high") - stock.get("low")) / stock.get("lastClose")));
                         changeUnit(stock);
                         stocks.add(stock);
                     } catch (Exception e) {
@@ -137,28 +145,30 @@ public class DailyDataProvider {
         return Lists.reverse(stocks);
     }
 
-    public static List<StockData> get(String symbol, String startDate, String stopDate){
-        Map<String,StockData> stockDatas = qfqData(symbol, startDate, stopDate);
+    public static List<StockData> get(String symbol, String startDate, String stopDate) {
+        Map<String, StockData> stockDatas = qfqData(symbol, startDate, stopDate);
 
         List<StockData> stockDataList = getDailyDataWithOutFQ(symbol, startDate, stopDate);
-//        String date = Utils.formatDate(stockDataList.get(stockDataList.size() - 1).date, "yyyyMMdd");
+        if(stockDatas.size()==0){
+            return stockDataList;
+        }
         ArrayList<String> dateList = Lists.newArrayList(stockDatas.keySet());
         Collections.sort(dateList);
-        String date = dateList.get(dateList.size()-1);
+        String date = dateList.get(dateList.size() - 1);
         double factor = stockDatas.get(date).get(StockConstants.FACTOR);//复权因子
-        for(int i =0;i<stockDataList.size();i++){
+        for (int i = 0; i < stockDataList.size(); i++) {
             StockData stockData = stockDataList.get(i);
-            date = Utils.formatDate(stockData.date,"yyyyMMdd");
+            date = Utils.formatDate(stockData.date, "yyyyMMdd");
             StockData stockDataFQ = stockDatas.get(date);
-            if(stockDataFQ!=null){
-                stockData.put(OPEN,Utils.formatDouble(stockDataFQ.get(OPEN)/factor));
-                stockData.put(CLOSE,Utils.formatDouble(stockDataFQ.get(CLOSE)/factor));
-                stockData.put(HIGH,Utils.formatDouble(stockDataFQ.get(HIGH)/factor));
-                stockData.put(LOW,Utils.formatDouble(stockDataFQ.get(LOW)/factor));
-                if(i>0){
-                    stockData.put(LAST_CLOSE,stockDataList.get(i-1).get(CLOSE));
+            if (stockDataFQ != null) {
+                stockData.put(OPEN, Utils.formatDouble(stockDataFQ.get(OPEN) / factor));
+                stockData.put(CLOSE, Utils.formatDouble(stockDataFQ.get(CLOSE) / factor));
+                stockData.put(HIGH, Utils.formatDouble(stockDataFQ.get(HIGH) / factor));
+                stockData.put(LOW, Utils.formatDouble(stockDataFQ.get(LOW) / factor));
+                if (i > 0) {
+                    stockData.put(LAST_CLOSE, stockDataList.get(i - 1).get(CLOSE));
                 }
-            }else{
+            } else {
                 //有时daily数据已经更新到最新，而复权数据还没有更新，这时去掉daily最新日期的数据，以复权数据源的日期数据为准
                 stockDataList.remove(i);
             }
@@ -167,20 +177,19 @@ public class DailyDataProvider {
     }
 
     private static void changeUnit(StockData stockData) {
-        stockData.put(VOLUME,stockData.get(VOLUME)/100);    //成交量,单位：手
-        stockData.put(AMOUNT,stockData.get(AMOUNT)/10000);  //成交金额,单位：万
-        stockData.put(TOTAL_VALUE,stockData.get(TOTAL_VALUE)/100000000);    //总市值,单位:亿
+        stockData.put(VOLUME, stockData.get(VOLUME) / 100);    //成交量,单位：手
+        stockData.put(AMOUNT, stockData.get(AMOUNT) / 10000);  //成交金额,单位：万
+        stockData.put(TOTAL_VALUE, stockData.get(TOTAL_VALUE) / 100000000);    //总市值,单位:亿
         stockData.put(MARKET_VALUE, stockData.get(MARKET_VALUE) / 100000000);   //流通市值,单位:亿
     }
 
     /**
-     *
      * @param symbol
      * @param startDate yyyyMMdd
      * @param stopDate  yyyyMMdd
      * @return
      */
-    public static String getPath(String symbol,String startDate,String stopDate) {
+    public static String getPath(String symbol, String startDate, String stopDate) {
         return String.format(DAILY_DATA_URL, Symbol.getSymbol(symbol, DAILY_DATA_URL), startDate, stopDate);
     }
 
@@ -188,8 +197,8 @@ public class DailyDataProvider {
         DateRange range = DateRange.getRange(5);
 
         Map<String, StockData> stockDataList1 = DailyDataProvider.qfqData("000008", range.start(), range.stop());
-        for(Map.Entry<String,StockData> stockDataEntry:stockDataList1.entrySet()){
-            System.out.println(stockDataEntry.getKey()+":"+stockDataEntry.getValue());
+        for (Map.Entry<String, StockData> stockDataEntry : stockDataList1.entrySet()) {
+            System.out.println(stockDataEntry.getKey() + ":" + stockDataEntry.getValue());
         }
     }
 }
