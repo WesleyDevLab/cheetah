00c72fd294ad8a2329243c46d4f445c3c8def7fe
牧仁 <xuqi.xq@alibaba-inc.com>
2015-11-04 23:40:46 +0800

    update

--- stock/src/main/java/com/zhaijiong/stock/strategy/Order.java
+++ stock/src/main/java/com/zhaijiong/stock/strategy/Order.java
@@ -1,9 +1,54 @@
 package com.zhaijiong.stock.strategy;
 
 /**
- * author: xuqi.xq
- * mail: xuqi.xq@alibaba-inc.com
+ * author: eryk
+ * mail: xuqi86@gmail.com
  * date: 15-10-23.
  */
 public class Order {
+    private String traderId;
+    private Execution execution;
+    private int status = 0; //0:未成交，1:成交
+
+    public Order(String traderId, Execution execution) {
+        this.traderId = traderId;
+        this.execution = execution;
+    }
+
+    public void done(){
+        this.status = 1;
+    }
+
+    public String getTraderId() {
+        return traderId;
+    }
+
+    public void setTraderId(String traderId) {
+        this.traderId = traderId;
+    }
+
+    public Execution getExecution() {
+        return execution;
+    }
+
+    public void setExecution(Execution execution) {
+        this.execution = execution;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "traderId='" + traderId + '\'' +
+                ", execution=" + execution +
+                ", status=" + status +
+                '}';
+    }
 }
