be5ee62ab698be139e81715eb8b5a9fc91269f00
牧仁 <xuqi.xq@alibaba-inc.com>
2016-04-11 22:27:40 +0800

    add stock filter condition

--- python/stocks.py
+++ python/stocks.py
@@ -19,10 +19,12 @@ df = pd.DataFrame
 def MACD(symbol):
     stocks = ts.get_hist_data(symbol, start='2015-01-01', end='2016-04-09')
     closes = stocks.sort_index().close.values
-    macd, macdsignal, macdhist = talib.MACD(closes, fastperiod=12, slowperiod=55, signalperiod=9)
-    for i in range(len(macd)):
-        print "close:%0.2f\tdif:%0.2f\tdea:%0.2f\tmacd:%0.2f" \
-              % (utils.f(closes[i]), utils.f(macd[i]), utils.f(macdsignal[i]), utils.f((macd[i] - macdsignal[i]) * 2))
+    # macd, macdsignal, macdhist = talib.MACD(closes, fastperiod=12, slowperiod=55, signalperiod=9)
+    macd = talib.MACD(closes, fastperiod=12, slowperiod=55, signalperiod=9)
+    # for i in range(len(macd)):
+    # print "close:%0.2f\tdif:%0.2f\tdea:%0.2f\tmacd:%0.2f" \
+    #           % (utils.f(closes[i]), utils.f(macd[i]), utils.f(macdsignal[i]), utils.f((macd[i] - macdsignal[i]) * 2))
+    return macd
 
 
 def list_stock():
@@ -30,6 +32,7 @@ def list_stock():
     stocks = ts.get_stock_basics()
     stocks = stocks[(stocks.pe < 100) & (stocks.pe > 0) & (stocks.totalAssets < 300000)]
     return stocks
+
 
 def get_basic(index, day=30):
     start_date = utils.get_start_date(day).strftime("%Y-%m-%d")
@@ -42,12 +45,13 @@ def get_basic(index, day=30):
     else:
         return
 
-    print stock_data[0:1]
-
     mean = stock_data.mean()
-    if 5 > mean.p_change > 1 and mean.turnover < 10:
-        print "%s,p_change=%f,turnover=%f,isHigh=%s" % (index, mean.p_change, mean.turnover, is_high)
+
+    if 5 > mean.p_change > 1 and mean.turnover < 10 and mean.close * 1.2 >= stock_data.ix[0].close:
+        print "%s,p_change=%f,turnover=%f,close=%f,1.2close=%0.2f %0.2f,isHigh=%s" % \
+              (index, mean.p_change, mean.turnover, mean.close, mean.close * 1.2, stock_data.ix[0].close, is_high)
         # print stock_data.describe()
+
 
 def condition():
     stocks = list_stock()
@@ -55,16 +59,19 @@ def condition():
     for stock in stock_list:
         get_basic(stock)
 
+
 if __name__ == "__main__":
     # stocks = list_stock((stocks.pe < 100) & (stocks.pe > 0) & (stocks.totalAssets < 300000))
     # print utils.is_working_day(datetime.datetime.now())
     # count = 0
     # for stock in stocks.index:
     # val = ts.get_realtime_quotes(stock)
-    #     if float(val.price) < 15 and float(val.price) > 0:
+    # if float(val.price) < 15 and float(val.price) > 0:
     #         print stock, float(val.price)
     #         count += 1;
     # print len(stocks),count
 
-    get_basic('300415')
-    # condition()
+    # macd = MACD('300145')
+    # print macd
+    # get_basic('300415')
+    condition()
